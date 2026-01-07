8f688c1fcefd2dd669788f341f9ba0d25dcfe9b3
BitForLi <miaomiao.emo@gmail.com>
2026-01-07 15:11:51 +1100

    delete from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,15 @@
-export const cart = [];
+export const cart = [ {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    priceCents: 1090,
+    quantity: 2,
+},
+{
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    name: "Wireless Bluetooth Over-Ear Headphones with Mic",
+    priceCents: 2599,
+    quantity: 1,
+}];
 
 export function addToCart(product) {
 
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart as mycart, addToCart} from '../data/cart.js'
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 const cart = [];
 
@@ -24,7 +25,7 @@ products.forEach((product) => {
             </div>
           </div>
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -79,9 +80,6 @@ function updateCartQuantity() {
     const product = products.find((p) => p.id === button.dataset.productId);
     addToCart(product);
     updateCartQuantity();
-
-    
-
 
   });
  });
